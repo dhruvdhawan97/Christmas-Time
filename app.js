ec0b7e66cae625a16ede10a40df05990aeb20d0b
Dhruv Dhawan <dhruv.dhawan97@gmail.com>
2025-12-25 04:48:58 -0500

    rollover to next year Christmas after 25th of December is over

--- app.js
+++ app.js
@@ -4,27 +4,33 @@ const hoursEl = document.getElementById("hours");
 const daysEl = document.getElementById("days");
 const yearEl = document.getElementById("current-year");
 
-function getChristmasDate(){
+
+let targetDate = getNextChristmasDate();
+function getNextChristmasDate(){
   const today = new Date();
   let year = today.getFullYear();
 
-  // Christmas date
-  let christmas = new Date(year, 11, 25);
+  // Christmas date to the end of christmas day
+  let christmas = new Date(year, 11, 25, 23, 59, 59);
 
   // christmas day passed switch to next year
   if(today>christmas){
     year++;
-    christmas= new Date(year, 11, 25);
+    christmas= new Date(year, 11, 25, 23, 59, 59);
   }
   yearEl.innerHTML = year;
   return christmas;
 }
 
 function countdown() {
-  const targetDate = getChristmasDate();
   const today = new Date();
 
   const totSeconds = Math.floor((targetDate - today) / 1000);
+//rollover to next christmas
+  if (totSeconds <=0){
+    targetDate = getNextChristmasDate();
+    totSeconds = Math.floor((targetDate- today)/1000);
+  }
   const seconds = Math.floor(totSeconds) % 60;
   const minutes = Math.floor(totSeconds / 60) % 60;
   const hours = Math.floor(totSeconds / 3600) % 24;
